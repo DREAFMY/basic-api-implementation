cbef07b14683935b7c56ab7b12fa1c1b01d7df10
Lu <1625196521@qq.com>
2020-09-21 00:26:50 +0800

    before test init

--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -39,40 +39,20 @@ public class UserControllerTest {
     @Test
     @Order(1)
     public void should_register_user() throws Exception {
-        User user = new User("haha","male","2343@a.com","1888888888",33,10);
+        User user = new User("haha","male","2343@a.com","18888888888",33,10);
         objectMapper.configure(USE_ANNOTATIONS, false);
         String jsonString = objectMapper.writeValueAsString(user);
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
         List<UserPO> all = userRepository.findAll();
-       // assertEquals(1, all.size());
         assertEquals("haha", all.get(0).getName());
-
-//        mockMvc.perform(get("/user"))
-//                .andExpect(jsonPath("$", hasSize(1)))
-//                .andExpect(jsonPath("$[0].name", is("hahaha")))
-//                .andExpect(jsonPath("$[0].gender", is("male")))
-//                .andExpect(jsonPath("$[0].email", is("123@a.com")))
-//                .andExpect(jsonPath("$[0].phone", is("18888888888")))
-//                .andExpect(jsonPath("$[0].age", is(18)))
-//                .andExpect(jsonPath("$[0].voteNum", is(10)))
-//                .andExpect(status().isOk());
-    }
-
-    @Test
-    @Order(2)
-    public void name_should_less_than_8() throws Exception {
-        User user = new User("haha","male","2343@a.com","1888888888",33,10);
-        String jsonString = objectMapper.writeValueAsString(user);
-        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
     }
 
     @Test
     @Order(3)
-    public void age_should_between_18_and_100() throws Exception {
-        User user = new User("haha","male","2343@a.com","1888888888",33,10);
+    public void name_should_less_than_8() throws Exception {
+        User user = new User("hahalalalalalal","male","2343@a.com","18888888888",33,10);
         String jsonString = objectMapper.writeValueAsString(user);
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
@@ -80,8 +60,8 @@ public class UserControllerTest {
 
     @Test
     @Order(4)
-    public void email_should_suit_format() throws Exception {
-        User user = new User("haha","male","2343@a.com","1888888888",33,10);
+    public void age_should_between_18_and_100() throws Exception {
+        User user = new User("haha","male","2343@a.com","18888888888",3,10);
         String jsonString = objectMapper.writeValueAsString(user);
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
@@ -89,8 +69,8 @@ public class UserControllerTest {
 
     @Test
     @Order(5)
-    public void phone_should_suit_format() throws Exception {
-        User user = new User("haha","male","2343@a.com","1888888888",33,10);
+    public void email_should_suit_format() throws Exception {
+        User user = new User("haha","male","2343a.com","18888888888",33,10);
         String jsonString = objectMapper.writeValueAsString(user);
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
@@ -98,13 +78,22 @@ public class UserControllerTest {
 
     @Test
     @Order(6)
+    public void phone_should_suit_format() throws Exception {
+        User user = new User("haha","male","2343@a.com","188888888",33,10);
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Order(2)
     public void should_get_user_by_id() throws Exception {
         mockMvc.perform(get("/user/1"))
-                .andExpect(jsonPath("$.userName",is("lulu")))
-                .andExpect(jsonPath("$.email",is("123@a.com")))
-                .andExpect(jsonPath("$.gender",is("female")))
+                .andExpect(jsonPath("$.name",is("haha")))
+                .andExpect(jsonPath("$.email",is("2343@a.com")))
+                .andExpect(jsonPath("$.gender",is("male")))
                 .andExpect(jsonPath("$.phone",is("18888888888")))
-                .andExpect(jsonPath("$.age",is(13)))
+                .andExpect(jsonPath("$.age",is(33)))
                 .andExpect(jsonPath("$.voteNum",is(10)))
                 .andExpect(status().isOk());
     }
